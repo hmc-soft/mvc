a9cb02eff090e76b2c692b32f35b5732da526a91
Ebben Feagan <ebben.feagan@gmail.com>
2015-11-09 13:40:50 -0600

    Updated Model get and added search/delete functions

--- app/Framework/Model.php
+++ app/Framework/Model.php
@@ -5,12 +5,10 @@ use HMC\Database\Connection;
 use HMC\Database\Table;
 
 /*
- * model - the base model
+ * The Base class for Models.
+ * Optionally acts like a simple ORM.
  *
- * @author David Carr - dave@simplemvcframework.com
- * @version 2.2
- * @date June 27, 2014
- * @date updated May 18 2015
+ * @author Ebben Feagan - ebben@hmc-soft.com
  */
 
 abstract class Model
@@ -26,15 +24,26 @@ abstract class Model
     private $values = null;
     private $dirty = false;
     private $new = true;
+    private $pk = null;
+    private $table = null;
 
     /**
     * This function is meant to be called from the derived constructor
     * with the name of the Table in the database to setup the simple ORM.
     */
     protected function alias($name = null) {
-      $table = Table::fromDb($name,$this->db);
-      if($table == null) return;
-      $fields = $table->fields();
+      $this->table = Table::fromDb($name,$this->db);
+      if($this->table == null) return;
+      $this->fields = $this->table->fieldNames();
+      foreach($this->table->fieldDetails() as $field) {
+        if($this->pk == null){
+          if(isset($field['primary'])) {
+            if($field['primary']) {
+              $this->pk = $field['name'];
+            }
+          }
+        }
+      }
       $this->name = $name;
     }
 
@@ -65,36 +74,100 @@ abstract class Model
 
     /**
     * Simple method to retrieve a single record for the model.
-    * @param $filter - the SQL WHERE statement to get the record, like "id = '1'"
+    * Array Example:
+    * `MyModel::get(array("parent" => 1, "type" => "menu"));`
+    * Becomes:
+    * `SELECT fields... FROM MyModel WHERE (parent = '1') AND (type = 'menu');`
+    * @param $id- the value of the primary key of the record to retrieve or an array of key => values that will form the where statement.
     */
-    public function get($filter) {
-      $this->values = $this->db->select(
-        "SELECT " . join(",",$this->fields) . " FROM {$this->name} WHERE $filter;"
+    public static function get($id) {
+      $ret = new self();
+      if($ret->pk == null) {
+        throw new \Exception("Primary key not defined for " . $ret->name);
+      }
+      $passArray = false;
+      if(is_array($id)) {
+        $passArray = true;
+        $where = '';
+        foreach($id as $key => $value) {
+          $where .= "({$key} = :{$key}) AND ";
+        }
+        $where = rtrim($where," AND ");
+      } else {
+        $where = "{$ret->pk} = :{$ret->pk}";
+      }
+      $ret->values = $ret->db->select(
+        "SELECT " . join(",",$ret->fields) . " FROM {$ret->name} WHERE $where;",
+        ($passArray ? $id : array($ret->pk => $id))
       );
-      return $this->values == null;
+      return $ret->values == null;
+    }
+
+    /**
+    * Run a query and get all matching objects.
+    * @param $query string containing the WHERE clause of a SQL statement (WHERE not needed), like key = :key AND key2 = :key2.
+    * @param $vals array containing key/value pairs to bind to where statement.
+    * @return array of Model objects with results.
+    */
+    public static function search($query,$vals) {
+      $ret = new self();
+      $results = $ret->db->select(
+        "SELECT " . join(",",$ret->fields) . " FROM {$ret->name} WHERE {$query}",
+        $vals
+      );
+      $retArray = array();
+      foreach($results as $result) {
+        $i = new self($ret->db,$result);
+        $retArray[] = $i;
+      }
+      return $retArray;
     }
 
     /**
     * Updates or Creates a new record if any changes have been made.
-    * @param $idFieldName - which field is the unique identifier, defaults to 'id'
     */
-    public function save($idFieldName = 'id') {
+    public function save() {
       if(!$this->dirty) return null;
+      if($this->pk == null) {
+        throw new \Exception("Primary key not defined for " . $this->name);
+      }
       $this->dirty = false;
       if($this->new) {
+        $this->new = false;
         return $this->db->insert($this->name, $this->values);
       } else {
-        return $this->db->update($this->name, $this->values, array($idFieldName => $this->values[$idFieldName]));
+        return $this->db->update($this->name, $this->values, array($this->pk => $this->values[$this->pk]));
       }
+    }
+
+    /**
+    * Deletes the current record.
+    */
+    public function delete() {
+      if($this->values == null) return;
+      if($this->pk == null) {
+        throw new \Exception("Primary key not defined for " . $this->name);
+      }
+      $this->db->delete($this->name, array($this->pk => $this->values[$this->pk]));
+      $this->dirty = false;
+      unset($this->values);
+      $this->values = null;
     }
 
     /**
      * create a new instance of the database helper
      */
-    public function __construct()
+    public function __construct($dbo = null,$valuesArray = null)
     {
-        //connect to PDO here.
+      //connect to PDO here.
+      if($dbo == null){
         $this->db = Connection::get();
+      } else {
+        $this->db = $dbo;
+      }
 
+      $this->alias(get_class($this));
+
+      $this->values = $valuesArray;
     }
 }
